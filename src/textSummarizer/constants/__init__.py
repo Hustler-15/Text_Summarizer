45d11a9342a67297b9bd94b98d5a6e23e4ef034b
Hustler-15 <ap644531@gmail.com>
2024-06-02 00:12:04 +0530

    CICD added

--- src/textSummarizer/constants/__init__.py
+++ src/textSummarizer/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("C:/Users/ap644/Projects/Text_Summarizer/config/config.yaml")
-PARAMS_FILE_PATH = Path("C:/Users/ap644/Projects/Text_Summarizer/params.yaml")
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
